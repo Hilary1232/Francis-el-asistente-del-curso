af0bebfbfcb714e790b1bd1e0f71fbb3fdc07180
Maria Venegas <m.venegasb98@gmail.com>
2019-12-30 20:31:55 -0600

    Ambiente con Webhook
    Se integró el uso de un webhook para el funcionamiento y desarrollo del bot

--- Francis/app.py
+++ Francis/app.py
@@ -3,10 +3,6 @@ from flask_restful import Resource, Api
 import modelo
 import json
 import collections
-import requests
-from bottle import (
-    run, post, response, request as bottle_request
-)
 
 #Crear motor para conectarse a SQLite3
 engine = modelo.engine
@@ -14,7 +10,6 @@ session = modelo.Session()
 app = Flask(__name__)
 api = Api(app)
 
-BOT_URL = 'https://api.telegram.org/bot1043017404:AAEZabTKNCf8csRbBVvNljrRZ8INL520ZLQ/'
 
 @app.route('/descargar', methods=["POST"])
 def descargarArchivoCsv():
@@ -27,67 +22,28 @@ def descargarArchivoCsv():
     result = transformar(file_contents)
 
     response = make_response(result)
-    response.headers["Content-Disposition"] = "attachment; filename=guión.csv"
+    response.headers["Content-Disposition"] = "attachment; filename=FRANCIS_CSV_FILE.csv"
     return response
 
 def transformar(text_file_contents):
     return text_file_contents.replace("=", ",")
 
-def get_chat_id(data):
-    """
-    Method to extract chat id from telegram request.
-    """
-    chat_id = data['message']['chat']['id']
-
-    return chat_id
-
-def get_message(data):
-    """
-    Method to extract message id from telegram request.
-    """
-    message_text = data['message']['text']
-
-    return message_text
-
-def send_message(prepared_data):
-    """
-    Prepared data should be json which includes at least `chat_id` and `text`
-    """
-    message_url = BOT_URL + 'sendMessage'
-    requests.post(message_url, json=prepared_data)  # don't forget to make import requests lib
-
-def change_text_message(text):
-    """
-    To enable turning our message inside out
-    """
-    return text[::-1]
-
-def prepare_data_for_answer(data):
-    answer = change_text_message(get_message(data))
-
-    json_data = {
-        "chat_id": get_chat_id(data),
-        "text": answer,
-    }
-
-    return json_data
-
-@post('/')
-def bot_question():
-    data = bottle_request.json
-
-    answer_data = prepare_data_for_answer(data)
-    send_message(answer_data)  # <--- function for sending answer
-
-    return response  # status 200 OK by default
-
 @app.route('/', methods=['GET']) #Cuando el href solo tenga un '/', que llegue a esta funcion y ejecute
-def home():
+def index():
     return render_template('index.html')
+
+@app.route('/home', methods=['GET','post']) #Cuando el href tenga un '/home', que llegue a esta funcion y ejecute
+def home():
+    return render_template('home.html')
 
 @app.route('/cursos', methods=['GET']) #Cuando la solicitud tiene un /cursos, devuelva la pagina de cursos
 def cursos():
     return render_template('cursos.html')
+
+@app.route('/get-key',methods=['POST'])
+def get_key():
+    bot_key = request.form['key']
+    #key = 1043017404:AAEZabTKNCf8csRbBVvNljrRZ8INL520ZLQ
 
 @app.route('/cursos-get') #Es llamada por Javascript, para mostrar la tabla de cursos de la base. 
 def getcursos():
@@ -109,6 +65,5 @@ def getcursos():
     js = json.dumps(lista_cursos)
     return js
 
-if __name__ == '__main__':
-    run(host='localhost', port=5000, debug=True)
+app.run(host='localhost', port=5001, debug=True)
 
--- Francis/bot.py
+++ Francis/bot.py
@@ -1,7 +1,8 @@
-import requests as requests
-import random
-
-url = 'https://api.telegram.org/bot1043017404:AAEZabTKNCf8csRbBVvNljrRZ8INL520ZLQ/'
+import requests
+from bottle import (
+    run, post, response, request as bottle_request
+)
+BOT_URL = 'https://api.telegram.org/bot1043017404:AAEZabTKNCf8csRbBVvNljrRZ8INL520ZLQ/'
 
 
 # create func that get chat id
@@ -15,35 +16,20 @@ def get_message_text(update):
     message_text = update["message"]["text"]
     return message_text
 
-
-# create function that get last_update
-def last_update(req):
-    response = requests.get(req + "getUpdates")
-    response = response.json()
-    result = response["result"]
-    total_updates = len(result) - 1
-    return result[total_updates]  # get last record message update
-
-
-# create function that let bot send message to user
 def send_message(chat_id, message_text):
     params = {"chat_id": chat_id, "text": message_text}
-    response = requests.post(url + "sendMessage", data=params)
+    response = requests.post(BOT_URL + "sendMessage", data=params)
     return response
 
-def lookup(update): #Se supone que aqui es la funcion donde entra al csv o a la base y busca la respuesta adecuada
-  message = get_message_text(update)
-  answer = send_message(get_chat_id(update),'Hilary perra!!')
+def lookup(data): #Se supone que aqui es la funcion donde entra al csv o a la base y busca la respuesta adecuada
+  message = get_message_text(data)
+  answer = send_message(get_chat_id(data),'Hilary es una perra!!')
+  return answer
 
-# create main function for navigate or reply message back
+@post('/')
 def main():
-    update_id = last_update(url)["update_id"]
-    while True:
-        update = last_update(url)
-        if update_id == update["update_id"]:
-            lookup(update)
-            update_id += 1
+    data = bottle_request.json
+    status = lookup(data)
+    return status  # status 200 OK by default
 
-
-# call the function to make it reply
-main()
+run(host='localhost', port=5000, debug=True)
