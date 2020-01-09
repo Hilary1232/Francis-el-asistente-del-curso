c4b8dad68c7d3a9547d15d6f2c09205ec75ce51a
Maria Venegas <m.venegasb98@gmail.com>
2020-01-09 17:23:15 -0600

    cambio direccion python

---
+++ Francis/venv/Scripts/cython-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\Maria\Desktop\Francis-el-asistente-del-curso\Francis\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'Cython==0.29.14','console_scripts','cython'
+__requires__ = 'Cython==0.29.14'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('Cython==0.29.14', 'console_scripts', 'cython')()
+    )
--- Francis/venv/Scripts/easy_install-3.8-script.py
+++ Francis/venv/Scripts/easy_install-3.8-script.py
@@ -1,4 +1,4 @@
-#!C:\Users\Maria\Desktop\Francis\venv\Scripts\python.exe
+#!C:\Users\Maria\Desktop\Francis-el-asistente-del-curso\Francis\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install-3.8'
 __requires__ = 'setuptools==40.8.0'
 import re
